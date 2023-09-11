324f5f609c8e71b4d052266779a74f396e124547
Bao Khanh Tran <ktbv.22@gmail.com>
2023-09-11 08:51:18 -0700

    added frontend

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,36 @@
-import React from "react"
+import React, { useState }from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
+
+
 const App = () => {
+    const [name, setName] = useState("")
+
+    const handleChange = (e) => {
+      setName(e.target.value)
+    }
+
+    const handleClick = () => {
+      setName("")
+    } 
+
   return (
     <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+      <h1 style={{color:'white', fontSize:'50px' }}>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+
+          <div className='input-label'>
+            <Label for="name">Enter your name</Label>
+          </div>
+          <Input value={name} onChange={handleChange}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div className='button-container'>
+        <ModalComponent inputName={name}/>
+        <Button onClick={handleClick}>Reset</Button>
+        </div>
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,35 @@
-import React from "react"
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import './ModalComponent.css'
 
-const ModalComponent = () => {
-  return <></>
+const ModalComponent = ({ inputName }) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+  return (
+    <div className='modal-container'>
+      <Button style={{backgroundColor:"#0dcaf0", width:'100px', padding:'10px'}} onClick={toggle}>
+        Click Me
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>Hello there {inputName}!!</ModalHeader>
+        <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
 }
 
 export default ModalComponent
